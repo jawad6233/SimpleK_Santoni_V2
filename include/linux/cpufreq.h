260e8164924025d918f6fb818fa0c5de61a0dc4f
kdrag0n <dragon@khronodragon.com>
2018-05-26 01:48:12 +0800

    sched: add various eas governors
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -549,6 +549,12 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARDSCHED)
+extern struct cpufreq_governor cpufreq_gov_alucardsched;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucardsched)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESSSCHED)
+extern struct cpufreq_governor cpufreq_gov_darknesssched;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darknesssched)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
